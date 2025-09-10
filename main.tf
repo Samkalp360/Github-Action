5a6898931e01b6ba43aa1933d39b983cda0ca579
Universal User1 <universaluser1@palanhare.onmicrosoft.com>
2025-09-10 23:46:18 +0530

    PiP method changed

--- main.tf
+++ main.tf
@@ -35,7 +35,8 @@ resource "azurerm_public_ip" "pip" {
   name                = "my-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "nsg" {
